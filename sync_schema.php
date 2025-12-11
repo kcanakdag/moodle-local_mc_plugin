855f665958a5634270b3d58e58ad92d5030b7a65
Kerem Can Akdag <kcanakdag26@gmail.com>
2025-12-11 16:59:17 +0100

    Add GPL license headers to all PHP files
    - Added standard Moodle GPL v3 header to all PHP files
    - Copyright year set to 2025
    - Headers follow Moodle coding standards
    - Includes package, copyright, and license information

--- sync_schema.php
+++ sync_schema.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Schema sync endpoint for MoodleConnect.
  * 
@@ -6,6 +21,8 @@
  * Also provides a standalone sync UI for advanced users.
  * 
  * @package    local_mc_plugin
+ * @copyright  2025 Kerem Canakdag
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once('../../config.php');
